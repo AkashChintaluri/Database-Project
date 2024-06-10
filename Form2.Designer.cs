d645975e0621205cc62b2b515e81bcdf865c68e3
Chintaluri Akash <akashchintaluriofficial@gmail.com>
2024-06-10 09:44:40 +0530

    added code for adding to database

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -42,41 +42,45 @@
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox5.Location = new System.Drawing.Point(213, 12);
+            this.textBox5.Location = new System.Drawing.Point(163, 12);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(272, 30);
             this.textBox5.TabIndex = 0;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox6.Location = new System.Drawing.Point(213, 48);
+            this.textBox6.Location = new System.Drawing.Point(163, 48);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(272, 30);
             this.textBox6.TabIndex = 1;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
             // 
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox7.Location = new System.Drawing.Point(213, 84);
+            this.textBox7.Location = new System.Drawing.Point(163, 84);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(272, 30);
             this.textBox7.TabIndex = 2;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
             // 
             // textBox8
             // 
             this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox8.Location = new System.Drawing.Point(213, 120);
+            this.textBox8.Location = new System.Drawing.Point(163, 120);
             this.textBox8.Multiline = true;
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(272, 81);
             this.textBox8.TabIndex = 3;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label5.Location = new System.Drawing.Point(99, 15);
+            this.label5.Location = new System.Drawing.Point(49, 15);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(64, 25);
             this.label5.TabIndex = 4;
@@ -86,7 +90,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label6.Location = new System.Drawing.Point(64, 51);
+            this.label6.Location = new System.Drawing.Point(14, 51);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(99, 25);
             this.label6.TabIndex = 5;
@@ -96,7 +100,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label7.Location = new System.Drawing.Point(103, 87);
+            this.label7.Location = new System.Drawing.Point(53, 87);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(60, 25);
             this.label7.TabIndex = 6;
@@ -106,7 +110,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label8.Location = new System.Drawing.Point(78, 140);
+            this.label8.Location = new System.Drawing.Point(28, 140);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(85, 25);
             this.label8.TabIndex = 7;
@@ -114,16 +118,17 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(189, 218);
+            this.button2.Location = new System.Drawing.Point(152, 218);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(142, 46);
             this.button2.TabIndex = 8;
             this.button2.Text = "SAVE";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form2
             // 
-            this.ClientSize = new System.Drawing.Size(528, 276);
+            this.ClientSize = new System.Drawing.Size(460, 276);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
--- Form2.cs
+++ Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,17 +18,45 @@ namespace Database_Project
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connString = "Data Source=AKASHPC;Initial Catalog=TDK;Integrated Security=True";
+            string query = "INSERT INTO Employee_Details (Name, PhoneNo, Email, Address) VALUES (@Name, @PhoneNumber, @Email, @Address)";
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", textBox5.Text);
+                    command.Parameters.AddWithValue("@PhoneNumber", textBox6.Text);
+                    command.Parameters.AddWithValue("@Email", textBox7.Text);
+                    command.Parameters.AddWithValue("@Address", textBox8.Text);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Data saved successfully!");
+        }
+
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void textBox6_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
         {
 
         }
